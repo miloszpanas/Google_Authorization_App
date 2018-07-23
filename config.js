cdb2a392f481ea92f014e217033a28a70595ebbd
Miłosz Panas <miloszpanas@gmail.com>
2018-07-23 19:48:35 +0200

    Add files and a completed task

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '102437586300-vudmus62vo3fug4jslmcdiqhe4irli0b.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '_Jsgd57f_8KxbXhCUi5B1KlJ',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
